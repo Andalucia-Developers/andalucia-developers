5d5af4cb68bb4bbb66513d212d15f3fdda4839e5
Juan José Salvador Piedra <juanjosalvador@netc.eu>
2018-01-12 16:54:14 +0100

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp   = require('gulp');
+var md2json = require('gulp-markdown-table-to-json');
+
+gulp.task('build', function () {
+  gulp.src('README.md')
+    .pipe(md2json())
+    .pipe(gulp.dest('./data.json'));
+});
+
+gulp.task('default', ['build'], function() {});
